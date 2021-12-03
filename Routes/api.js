51add1eb644dd6a931fc239ff612fee0acccebe6
shahid1223 <ms413959@gmail.com>
2021-12-02 19:37:11 -0800

    Added validation to api

--- Routes/api.js
+++ Routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Data = require('../modals/data')
+const { body, validationResult } = require('express-validator');
 
 router.get('/', async (req,res) => {
     try {
@@ -22,8 +23,21 @@ router.get('/getdatawithid/:id', async (req,res) => {
     
 })
 
-router.post('/creatdata', async (req,res) => {
+router.post('/creatdata', [
+    body('firstname',"First Name atleast have 5 char").isLength({ min: 3 }),
+    body('lastname',"Last Name atleast have 5 char").isLength({ min: 3 }),
+    // body('age',"Age above 18 requires").isLength({ min: 18 }),
+    body('email',"Enter a Valid Email").isEmail()
+] ,async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     try {
+        let ser = await Data.findOne({email: req.body.email});
+        if(ser){
+            return res.status(400).json({error:"Sorry a user with this email is already exist"})
+        }
         const data = new Data({
             firstname : req.body.firstname,
             lastname : req.body.lastname,
@@ -38,7 +52,7 @@ router.post('/creatdata', async (req,res) => {
     
 })
 
-router.put('/updatedata/:id', async (req,res) => {
+router.put('/updatedata/:id' ,async (req,res) => {
     try {
         const data = await Data.findById(req.params.id)
         data.firstname = req.body.firstname
